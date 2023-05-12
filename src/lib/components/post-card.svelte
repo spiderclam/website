3118a100fc81d08d1dab65e9222856e62b6f2af9
RWOverdijk <r.w.overdijk@gmail.com>
2023-05-12 16:53:32 +0200

    fix(components): fix vertical spacing

--- src/lib/components/post-card.svelte
+++ src/lib/components/post-card.svelte
@@ -6,20 +6,21 @@
 </script>
 
 <article
-	class="rounded-lg border border-slate-100 p-4 shadow-sm transition hover:shadow-lg dark:border-slate-800 dark:shadow-slate-800/25 sm:p-6 backdrop-blur-xl bg-white/5 hover:bg-white/10"
+	class="rounded-lg border border-slate-100 p-4 shadow-sm transition hover:shadow-lg flex flex-col justify-between dark:border-slate-400 dark:shadow-slate-400/25 sm:p-6 backdrop-blur-xl bg-white/5 hover:bg-white/10"
 >
-	<a href={`blog/${post.slug}`} class="no-underline">
-		<h3 class="mt-0.5">
-			{post.meta.title}
-		</h3>
-	</a>
+	<div class="mb-2">
+		<a href={`blog/${post.slug}`} class="no-underline">
+			<h3 class="mt-0.5">
+				{post.meta.title}
+			</h3>
+		</a>
 
-	<PostMeta {post} />
+		<PostMeta {post} />
 
-	<p class="mt-4 mb-2 text-sm leading-relaxed line-clamp-3">
-		{post.meta.preview}
-	</p>
-
+		<p class="mt-4 mb-2 text-sm leading-relaxed line-clamp-3">
+			{post.meta.preview}
+		</p>
+	</div>
 	<a href={`blog/${post.slug}`} class="group inline-flex items-center gap-1 text-sm font-medium">
 		Read post
 		<span aria-hidden="true" class="block transition group-hover:translate-x-0.5"> &rarr; </span>
