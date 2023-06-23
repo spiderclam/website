3c6dc6e6ea7a6b420685ddefb24dadae109cac52
RWOverdijk <r.w.overdijk@gmail.com>
2023-06-23 09:43:02 +0200

    feat(about): add more about me, work towards cv

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -10,12 +10,12 @@
 	<meta name="description" content="Spiderclam's gallery" />
 </svelte:head>
 
-<PageContainer title="Gallery">
-	<div class="grid grid-cols-2 md:grid-cols-3 gap-4">
+<PageContainer title="Gallery" size='huge'>
+	<div class="grid sm:grid-cols-2 lg:grid-cols-3 gap-4">
 		{#each data.gallery as item}
 				<a href={`gallery/${item.slug}`} class='relative shadow-sm hover:shadow-lg'>
 					<img class="max-w-full m-0 rounded-lg" src={item.meta.thumbnail} alt="">
-					<div class="h-auto max-w-full rounded-lg text-base opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 bg-black/50 flex justify-center items-center text-white">{item.meta.title}</div>
+					<div class="h-auto max-w-full rounded-lg text-base opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 bg-black/50 flex justify-center items-center text-white p-4">{item.meta.title}</div>
 				</a>
 		{/each}
 	</div>
